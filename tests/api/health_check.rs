8d81472fcf9b58525e63b79ea829ff72f9fd4abe
renato <renato.hermoza@pucp.edu.pe>
2021-09-13 14:50:32 +0930

    pass client_stays_alive_if_responds_pings

--- tests/api/health_check.rs
+++ tests/api/health_check.rs
@@ -1,6 +1,7 @@
 use crate::helpers::spawn_app;
+use actix_web_actors::ws;
 use awc::Client;
-use futures::StreamExt;
+use futures::{SinkExt, StreamExt};
 use std::time::Duration;
 
 #[actix_rt::test]
@@ -72,18 +73,48 @@ async fn client_disconnects_after_x_milliseconds() {
     assert!(disconnected, "Server did not disconnect.")
 }
 
-// connection
-//     .send(awc::ws::Message::Ping("".into()))
-//     .await
-//     .unwrap();
+#[actix_rt::test]
+async fn client_stays_alive_if_responds_pings() {
+    // Arrange
+    let app = spawn_app().await;
 
-// let response = connection.next().await;
-// tracing::info!("==> {:?}", response);
+    // Act
+    let (_response, mut connection) = Client::new()
+        .ws(format!("{}/ws/", app.address))
+        .connect()
+        .await
+        .expect("Failed to connect to websocket.");
 
-// connection
-//     .send(awc::ws::Message::Text("Echo".into()))
-//     .await
-//     .unwrap();
+    let sleep = tokio::time::sleep(Duration::from_millis(500));
+    tokio::pin!(sleep);
+    let mut disconnected = false;
 
-// let response = connection.next().await;
-// tracing::info!("==> {:?}", response);
+    // Ping
+    loop {
+        tokio::select! {
+            msg = connection.next() => {
+                tracing::info!("==> {:?}", msg);
+                match msg {
+                    Some(Ok(ws::Frame::Ping(msg))) => {
+                        connection
+                            .send(awc::ws::Message::Pong(msg))
+                            .await
+                            .expect("Failed to send Pong message.");
+                    }
+                    None => {
+                        disconnected = true;
+                        break;
+                    }
+                    _ => {}
+                }
+            }
+            _ = &mut sleep => {
+                tracing::info!("Timeout!");
+                break;
+            }
+        }
+    }
+
+    // Assert
+    assert!(!disconnected, "Server disconnected.")
+}
